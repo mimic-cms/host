ab7f9825333342ece4a7ef0dc084afeade2a4f3f
Michaela Isaacs <mail.m.isaacs@gmail.com>
2024-01-25 14:26:51 -0500

    feat: add example GH API connection

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,9 +1,36 @@
+import { request } from '@octokit/request';
+
 export const handler = async () => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Hello World!',
-      }),
-    }
+  const rand = Math.floor(Math.random() * 10)
+  const result = await request('GET /repos/{owner}/{repo}/contents/{path}', {
+    headers: {
+      authorization: `token ${process.env.GITHUB_PAT}`,
+    },
+    owner: 'mimic-cms',
+    repo: 'host',
+    path: 'README.md'
+  }).then(({ data: { sha }}) => {
+    return request('PUT /repos/{owner}/{repo}/contents/{path}', {
+      headers: {
+        authorization: `token ${process.env.GITHUB_PAT}`,
+      },
+      owner: 'mimic-cms',
+      repo: 'host',
+      path: 'README.md',
+      message: `chore: GH Connection Test ${rand}`,
+      committer: {
+        name: 'MimicCMS',
+        email: 'mail.m.isaacs@gmail.com'
+      },
+      sha,
+      content: btoa(`# host GH Connection Test ${rand}`)
+    })
+  })
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello World',
+    }),
   }
-  
+}
